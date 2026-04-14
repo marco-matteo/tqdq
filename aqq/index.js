641f0adcdd17bc7900ec8aee37513f94256992ce
Sandro Kossel <sandro.kossel@ergon.ch>
2026-04-14 14:54:12 +0200

    Display last login information in the welcome message

--- aqq/index.js
+++ aqq/index.js
@@ -5,7 +5,8 @@ const security = require('./fw/security');
 async function getHtml(req, res) {
     let taskListHtml = await tasklist.html(req);
     let username = security.escapeHTML(req.session.username);
-    return `<h2>Welcome, `+username+`!</h2>` + taskListHtml + '<hr />' + bgSearch.html(req, res);
+    let lastLogin = req.session.lastLogin ? `<p style="font-size: 0.8em; color: #666;">Your last login was on: ${new Date(req.session.lastLogin).toLocaleString()}</p>` : '<p style="font-size: 0.8em; color: #666;">Welcome! This is your first login.</p>';
+    return `<h2>Welcome, `+username+`!</h2>` + lastLogin + taskListHtml + '<hr />' + bgSearch.html(req, res);
 }
 
 module.exports = {
